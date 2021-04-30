a2a58dd291f144313894353ed9463ade6b55c71e
Siddhartha Gurung López <siddhartha@MacBook-Pro-de-Siddhartha.local>
2021-04-30 12:08:52 +0200

    asdasd

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Lya_zelda",
-    version="0.0.01",
+    version="0.0.03",
     author="Siddhartha Gurung Lopez",
     author_email="gurung.lopez@gmail.com",
     description="Fast Lyman alpha Radiative Transfer for everyone!",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sidgurun/LyaRT-Grid",
-    download_url = 'https://github.com/sidgurun/Lya_zelda/archive/refs/tags/0.0.01.tar.gz',
+    download_url = 'https://github.com/sidgurun/Lya_zelda/archive/refs/tags/0.0.03.tar.gz',
     packages=setuptools.find_packages(),
     #install_requires=[ 'sklearn>=20.0' ],
     #scikit-learn==0.22.1
